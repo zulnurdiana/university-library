55b29fd2fdbb847d16c11c73fdfa8908a022dbc6
Zulfikar Nurdiana <66067543+zulnurdiana@users.noreply.github.com>
2025-04-13 19:09:42 +0700

    add: theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "ibm-plex-sans": ["IBM Plex Sans", "sans-serif"],
+        "bebas-neue": ["var(--bebas-neue)"],
+      },
       colors: {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
@@ -20,10 +24,10 @@ export default {
           DEFAULT: "hsl(var(--popover))",
           foreground: "hsl(var(--popover-foreground))",
         },
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
+        // primary: {
+        //   DEFAULT: "hsl(var(--primary))",
+        //   foreground: "hsl(var(--primary-foreground))",
+        // },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
@@ -50,11 +54,60 @@ export default {
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
         },
+        primary: {
+          DEFAULT: "#E7C9A5",
+          admin: "#25388C",
+        },
+        green: {
+          DEFAULT: "#027A48",
+          100: "#ECFDF3",
+          400: "#4C7B62",
+          500: "#2CC171",
+          800: "#027A48",
+        },
+        red: {
+          DEFAULT: "#EF3A4B",
+          400: "#F46F70",
+          500: "#E27233",
+          800: "#EF3A4B",
+        },
+        blue: {
+          100: "#0089F1",
+        },
+        light: {
+          100: "#D6E0FF",
+          200: "#EED1AC",
+          300: "#F8F8FF",
+          400: "#EDF1F1",
+          500: "#8D8D8D",
+          600: "#F9FAFB",
+          700: "#E2E8F0",
+          800: "#F8FAFC",
+        },
+        dark: {
+          100: "#16191E",
+          200: "#3A354E",
+          300: "#232839",
+          400: "#1E293B",
+          500: "#0F172A",
+          600: "#333C5C",
+          700: "#464F6F",
+          800: "#1E2230",
+        },
+        gray: {
+          100: "#CBD5E1",
+        },
+      },
+      screens: {
+        xs: "480px",
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
+      },
+      backgroundImage: {
+        pattern: "url('/images/pattern.webp')",
       },
     },
   },
